55f183b9280f7a03daf79a94bc4c10b043b4f6d4
nathan38btc <natmaure354@gmail.com>
2022-03-22 19:48:35 +0100

    changes

--- code.py
+++ code.py
@@ -1,10 +1,31 @@
+import json
+from urllib import request
 import pymongo
 from pymongo import MongoClient
+import requests
 
-cluster = MongoClient('localhost', 27017)
+# cluster = MongoClient('localhost', 27017)
 
-db = cluster["dbbinance"]
+# db = cluster["dbbinance"]
 
-collection = db["bitcoin"]
+# collection = db["bitcoin"]
 
-collection.insert_one({"test":"test"})
+# collection.insert_one({"test":"test"})
+
+
+# data production ( Binance ) :
+
+# Bitcoin Data 
+
+url ="https://api1.binance.com/api/v3/trades"
+
+param = {
+    "symbol":"BTCUSDT"
+}
+
+requete = requests.get(url = url, params=param)
+
+if(requete.status_code==200):
+    print(requete.json())
+else:
+    print("error")
